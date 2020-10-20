0e75d6274cf1e17a3de4f3b31e5e14a16f56ae5d
eliasroebl <eliasroebl@gmail.com>
2020-10-20 10:56:19 +0200

    Added JobApplication Entity

---
+++ src/main/java/com/prakti/entity/JobApplication.java
@@ -0,0 +1,21 @@
+package com.prakti.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "JOB_APPLICATION", schema = "prakticom")
+public class JobApplication extends PanacheEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STUDENT_ID", referencedColumnName = "id")
+    public Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "JOB_POSTING_ID", referencedColumnName = "id")
+    public JobPosting jobPosting;
+
+    @Column
+    public Date postingDate;
+}
