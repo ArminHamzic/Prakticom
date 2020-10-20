b1ee88518a437322210bbf65d2c8b550893a6927
eliasroebl <eliasroebl@gmail.com>
2020-10-20 11:43:15 +0200

    Added Enum of Possible Purposes of Documents and Pictures

---
+++ src/main/java/com/prakti/entity/DocumentEntities/PossibleDocumentPurposes.java
@@ -0,0 +1,13 @@
+package com.prakti.entity.DocumentEntities;
+
+public enum PossibleDocumentPurposes {
+    CurriculumVitae,
+    ProfilePicture,
+    Certificate,
+    MotivationLetter,
+    Vacancy,
+    CompanyPicture,
+    ProfileBackground
+
+
+}
