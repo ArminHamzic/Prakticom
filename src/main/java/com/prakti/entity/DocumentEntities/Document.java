d3a23317b6759f243dc279a959a8aacbcf7221b8
eliasroebl <eliasroebl@gmail.com>
2020-10-20 11:50:24 +0200

    Added Document Entity

---
+++ src/main/java/com/prakti/entity/DocumentEntities/Document.java
@@ -0,0 +1,21 @@
+package com.prakti.entity.DocumentEntities;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DOCUMENT", schema = "PRAKTICOM")
+public class Document extends PanacheEntity {
+    @Column(name = "DOCUMENT_URL")
+    public String documentUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MIME_TYPE_ID", referencedColumnName = "id")
+    public MimeType mimeType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DOCUMENT_PURPOSE_ID", referencedColumnName = "id")
+    public DocumentPurpose documentPurpose;
+
+}
