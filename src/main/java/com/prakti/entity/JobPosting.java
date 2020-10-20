fc100276950a0dd42fa3507b26d5a64f0039b1e3
eliasroebl <eliasroebl@gmail.com>
2020-10-20 10:51:03 +0200

    implemented JobPosting

--- src/main/java/com/prakti/entity/JobPosting.java
+++ src/main/java/com/prakti/entity/JobPosting.java
@@ -2,11 +2,33 @@ package com.prakti.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "JOB_POSTING", schema = "PRAKTICOM")
 public class JobPosting extends PanacheEntity {
+    @Column
     public String jobTitle;
+    @Column
+    public String jobDescription;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPANY_ID", referencedColumnName = "id")
+    public Company company;
+    @Column
+    public Date postingDate;
+    @Column
+    public String timeSpan;
+
+    public JobPosting CopyProperties(JobPosting other){
+        if(other.id != null){
+            jobTitle = other.jobTitle;
+            jobDescription = other.jobDescription;
+            company = other.company;
+            postingDate = other.postingDate;
+            timeSpan = other.timeSpan;
+        }
+        return this;
+    }
+
 }
