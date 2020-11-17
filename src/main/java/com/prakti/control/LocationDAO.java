8a4e15a6064f713ac0bbfb49cc0b23912f6d973f
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-11-17 09:53:35 +0100

    adding LocationDAO

---
+++ src/main/java/com/prakti/control/LocationDAO.java
@@ -0,0 +1,39 @@
+package com.prakti.control;
+
+import com.prakti.model.Location;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@ApplicationScoped
+public class LocationDAO implements PanacheRepository<Location> {
+
+    public List<Location> findAllLocations(){
+        return Location.findAll().list();
+    }
+
+    public Location findLocationById(Long id){
+        Optional<Location> optionalLocation = Location.findByIdOptional(id);
+        return optionalLocation.orElseThrow(NotFoundException::new);
+    }
+
+    public Location persistLocation(Location location){
+        persist(location);
+        return location;
+    }
+
+    public void updateLocation(Long id, Location location){
+        Location updateLocation = findLocationById(id);
+        updateLocation.CopyProperties(location);
+        persist(updateLocation);
+    }
+
+    public void deleteLocation(Long id){
+        delete(findLocationById(id));
+    }
+}
