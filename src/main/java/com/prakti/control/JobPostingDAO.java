59f12371bdfe97a244e461b289eee1683f59a4e6
eliasroebl <eliasroebl@gmail.com>
2020-11-17 09:28:43 +0100

    Implemented Data Access Object of JobPosting

--- src/main/java/com/prakti/control/JobPostingDAO.java
+++ src/main/java/com/prakti/control/JobPostingDAO.java
@@ -1,4 +1,48 @@
 package com.prakti.control;
 
-public class JobPostingDAO {
+import java.util.List;
+import java.util.Optional;
+
+import com.prakti.model.Company;
+import com.prakti.model.JobPosting;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.vertx.reactivex.ext.shell.system.Job;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
+
+@Transactional
+@ApplicationScoped
+public class JobPostingDAO implements PanacheRepository<JobPosting> {
+
+    public List<JobPosting> findAllJobPostings(){
+        return JobPosting.findAll().list();
+    }
+
+    public JobPosting findJobPostingById(Long id){
+        Optional<JobPosting> optionalJobPosting = JobPosting.findByIdOptional(id);
+        return optionalJobPosting.orElseThrow(NotFoundException::new);
+    }
+
+
+    public JobPosting findJobPostingByCompanyId(Company company){
+        Optional<JobPosting> optionalJobPosting = find("company_id",company.id).singleResultOptional();
+        return optionalJobPosting.orElseThrow(NotFoundException::new);
+    }
+
+    public JobPosting persistJobPosting(JobPosting jobPosting){
+        persist(jobPosting);
+        return jobPosting;
+    }
+
+    public void updateJobPosting(Long id, JobPosting jobPosting){
+        JobPosting updateJobPosting = findJobPostingById(id);
+        updateJobPosting.CopyProperties(jobPosting);
+        persist(updateJobPosting);
+    }
+
+    public void deleteJobPosting(Long id){
+        delete(findJobPostingById(id));
+    }
 }
