bfd68e00cdbd2bd38cce2c5ef672c621a3b4276d
eliasroebl <eliasroebl@gmail.com>
2020-11-23 00:09:42 +0100

    added OneToMany Relations to Company

--- src/main/java/com/prakti/model/Company.java
+++ src/main/java/com/prakti/model/Company.java
@@ -1,10 +1,14 @@
 package com.prakti.model;
 
+import com.prakti.model.DocumentEntities.StudentDocument;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "COMPANY", schema = "PRAKTICOM")
@@ -22,6 +26,16 @@ public class Company extends PanacheEntity {
     @Column(unique = true)
     public String url;
 
+    @OneToMany(mappedBy = "company_documents")
+    public List<StudentDocument> documents = new ArrayList<>();
+
+    @OneToMany(mappedBy = "job_application")
+    public List<JobApplication> jobApplications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "job_posting")
+    public List<JobPosting> jobPostings = new ArrayList<>();
+
+
     public Company CopyProperties(Company other){
         if(other.id != null){
             name = other.name;
@@ -30,6 +44,9 @@ public class Company extends PanacheEntity {
             contactEmail = other.contactEmail;
             contactPhoneNumber = other.contactPhoneNumber;
             url = other.url;
+            documents = other.documents;
+            jobApplications = other.jobApplications;
+            jobPostings = other.jobPostings;
         }
         return this;
     }
