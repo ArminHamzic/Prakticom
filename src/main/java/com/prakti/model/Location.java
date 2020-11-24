b653234cca438c8f39ee9c21b4eee90cfeedfd28
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-11-24 14:09:18 +0100

    creating http file for locations

--- src/main/java/com/prakti/model/Location.java
+++ src/main/java/com/prakti/model/Location.java
@@ -7,25 +7,30 @@ import javax.persistence.*;
 @Entity
 @Table(name = "LOCATION", schema = "PRAKTICOM")
 public class Location extends PanacheEntity {
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "COMPANY_ID", referencedColumnName = "ID")
     public Company company;
-    @Column
-    public String country;
-    @Column(name = "ZIP_CODE")
-    public String zipCode;
+
     @Column(name = "STREET_NAME")
     public String streetName;
-    @Column(name = "STREET_NUMBER")
-    public int streetNumber;
+
+    @Column(name = "CITY")
+    public String city;
+
+    @Column(name = "ZIP_CODE")
+    public String zipCode;
+
+    @Column(name = "COUNTRY")
+    public String country;
 
     public Location CopyProperties(Location other){
         if(other.id != null){
             company = other.company;
-            country = other.country;
-            zipCode = other.zipCode;
             streetName = other.streetName;
-            streetNumber = other.streetNumber;
+            city = other.city;
+            zipCode = other.zipCode;
+            country = other.country;
         }
         return this;
     }
