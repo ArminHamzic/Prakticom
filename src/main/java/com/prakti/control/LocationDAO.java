8772d116b6fb1554f72640eeb01224bc844c62e5
eliasroebl <eliasroebl@gmail.com>
2020-11-24 11:25:07 +0100

    added findLocationByCompanyId

--- src/main/java/com/prakti/control/LocationDAO.java
+++ src/main/java/com/prakti/control/LocationDAO.java
@@ -1,9 +1,12 @@
 package com.prakti.control;
 
+import com.prakti.model.Company;
 import com.prakti.model.Location;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.NotFoundException;
 import java.util.List;
@@ -13,6 +16,9 @@ import java.util.Optional;
 @ApplicationScoped
 public class LocationDAO implements PanacheRepository<Location> {
 
+    @Inject
+    CompanyDAO companyRepository;
+
     public List<Location> findAllLocations(){
         return Location.findAll().list();
     }
@@ -20,6 +26,13 @@ public class LocationDAO implements PanacheRepository<Location> {
     public Location findLocationById(Long id){
         Optional<Location> optionalLocation = Location.findByIdOptional(id);
         return optionalLocation.orElseThrow(NotFoundException::new);
+    }
+
+    public List<Location> findLocationsByCompanyId(Long companyId){
+        Company company = companyRepository.findCompanyById(companyId);
+        List<Location> locations = Location.find("company_id", company.id).list();
+        if(locations.size() == 0){throw new NotFoundException();}
+        else{return locations;}
     }
 
     public Location persistLocation(Location location){
