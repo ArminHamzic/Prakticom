8d5427f39810c068af2d08da9d5d75890b0b3208
eliasroebl <eliasroebl@gmail.com>
2020-11-29 06:33:19 +0100

    added Contract for Location

---
+++ frontend/src/app/contracts/location.ts
@@ -0,0 +1,9 @@
+export interface Location{
+    id: number;
+    country: string;
+    streetName: string;
+    city: string;
+    zipCode: string;
+    // company: ICompany;
+    companyId: number;
+}
