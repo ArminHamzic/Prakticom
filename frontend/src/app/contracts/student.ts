67802494d733afc0f256d22565384eae8fed304d
eliasroebl <eliasroebl@gmail.com>
2020-11-29 06:13:57 +0100

    added Contract for Student

---
+++ frontend/src/app/contracts/student.ts
@@ -0,0 +1,9 @@
+export interface IStudent{
+    id: number;
+    birthdate: Date;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    school: string;
+}
