39bd7196d0106eecd39eb676a6fc34bddb9806a8
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-01 14:18:24 +0100

    moving services to shared folder

--- frontend/src/services/GenericHttpService/HttpCrudOperations.ts
+++
@@ -1,9 +0,0 @@
-import {Observable} from 'rxjs';
-
-export interface HttpCrudOperations<T, ID> {
-    get(id: ID): Observable<T>;
-    getAll(): Observable<T[]>;
-    save(t: T): Observable<T>;
-    update(id: ID, t: T): Observable<T>;
-    delete(id: ID): Observable<any>;
-}
