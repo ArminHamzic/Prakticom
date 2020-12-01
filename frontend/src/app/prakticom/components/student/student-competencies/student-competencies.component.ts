5be408acc0dd49dc7026189efca9d71831ba4a15
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-01 14:20:56 +0100

    moving other components from student

---
+++ frontend/src/app/prakticom/components/student/student-competencies/student-competencies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-competencies',
+  templateUrl: './student-competencies.component.html',
+  styleUrls: ['./student-competencies.component.css']
+})
+export class StudentCompetenciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/shared/contracts/job.ts
@@ -0,0 +1,9 @@
+export interface IJob{
+  id: number;
+  image: string;
+  title: string;
+  duration: string;
+  description: string;
+  website: string;
+  postingDate: string;
+}
