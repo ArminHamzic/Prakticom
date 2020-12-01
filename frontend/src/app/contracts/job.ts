f87b3d7c2b1c602fa1f578b641f6e1c43f4b6a0f
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-01 14:17:04 +0100

    moving contracts to shared folder

--- frontend/src/app/contracts/job.ts
+++
@@ -1,9 +0,0 @@
-export interface IJob{
-  id: number;
-  image: string;
-  title: string;
-  duration: string;
-  description: string;
-  website: string;
-  postingDate: string;
-}
--- frontend/src/app/contracts/location.ts
+++
@@ -1,9 +0,0 @@
-export interface ILocation{
-    id: number;
-    country: string;
-    streetName: string;
-    city: string;
-    zipCode: string;
-    // company: ICompany;
-    companyId: number;
-}
