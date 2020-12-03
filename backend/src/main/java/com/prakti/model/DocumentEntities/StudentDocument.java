08acfb6b936a6c056261e3cf7bc7fa09ef1d9b60
eliasroebl <eliasroebl@gmail.com>
2020-12-03 03:23:18 +0100

    updated CompanyDocument & StudentDocument Model

--- backend/src/main/java/com/prakti/model/DocumentEntities/StudentDocument.java
+++ backend/src/main/java/com/prakti/model/DocumentEntities/StudentDocument.java
@@ -1,18 +1,34 @@
 package com.prakti.model.DocumentEntities;
 
 import com.prakti.model.Student;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
+import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "STUDENT_DOCUMENT", schema = "PRAKTICOM")
-public class StudentDocument extends PanacheEntity {
+public class StudentDocument extends PanacheEntityBase {
 
+
+    @Id
+    @SequenceGenerator(
+            name ="studentDocumentSequence",
+            sequenceName = "student_document_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "studentDocumentSequence"
+    )
+    public Long id;
+
+    @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DOCUMENT_ID", referencedColumnName = "id")
     public Document document;
 
+    @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "STUDENT_ID", referencedColumnName = "id")
     public Student student;
