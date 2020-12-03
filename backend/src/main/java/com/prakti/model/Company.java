b9ffa13d0cc1af3ed5a1baa21034956b7b93d3c0
eliasroebl <eliasroebl@gmail.com>
2020-12-03 02:47:46 +0100

    updated Company Model

--- backend/src/main/java/com/prakti/model/Company.java
+++ backend/src/main/java/com/prakti/model/Company.java
@@ -1,29 +1,44 @@
 package com.prakti.model;
 
 import com.prakti.model.DocumentEntities.CompanyDocument;
-import com.prakti.model.DocumentEntities.StudentDocument;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "COMPANY", schema = "PRAKTICOM")
-public class Company extends PanacheEntity {
+public class Company extends PanacheEntityBase implements Serializable {
 
+    @Id
+    @SequenceGenerator(
+            name ="companySequence",
+            sequenceName = "company_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "companySequence"
+    )
+    public Long id;
     @Column(unique = true)
     public String name;
     @Column
+    @Size(max=2000)
     public String description;
     @Column(name = "CONTACT_NAME")
     public String contactName;
-    @Column(unique = true, name = "CONTACT_EMAIL")
+    @Column(name = "CONTACT_EMAIL", unique = true)
     public String contactEmail;
     @Column(name = "CONTACT_PHONE_NUMBER")
     public String contactPhoneNumber;
+    //@URL
     @Column(unique = true)
     public String url;
+
 
     @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
     public List<CompanyDocument> documents = new ArrayList<>();
@@ -35,17 +50,13 @@ public class Company extends PanacheEntity {
     public List<Location> locations = new ArrayList<>();
 
 
-    public Company CopyProperties(Company other){
-        if(other.id != null){
-            name = other.name;
-            description = other.description;
-            contactName = other.contactName;
-            contactEmail = other.contactEmail;
-            contactPhoneNumber = other.contactPhoneNumber;
-            url = other.url;
-            documents = other.documents;
-            jobPostings = other.jobPostings;
-        }
-        return this;
+    public void CopyProperties(Company other){
+
+        this.name = other.name;
+        this.description = other.description;
+        this.contactName = other.contactName;
+        this.contactEmail = other.contactEmail;
+        this.contactPhoneNumber = other.contactPhoneNumber;
+        this.url = other.url;
     }
 }
