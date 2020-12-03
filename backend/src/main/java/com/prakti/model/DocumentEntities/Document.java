d51462c88e63d9b5c04b2f7d91c4327b67b13088
eliasroebl <eliasroebl@gmail.com>
2020-12-03 03:24:11 +0100

    updated Document Model

--- backend/src/main/java/com/prakti/model/DocumentEntities/Document.java
+++ backend/src/main/java/com/prakti/model/DocumentEntities/Document.java
@@ -1,19 +1,38 @@
 package com.prakti.model.DocumentEntities;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.hibernate.validator.constraints.URL;
 
+import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "DOCUMENT", schema = "PRAKTICOM")
-public class Document extends PanacheEntity {
+public class Document extends PanacheEntityBase {
+
+    @Id
+    @SequenceGenerator(
+            name ="documentSequence",
+            sequenceName = "document_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "documentSequence"
+    )
+    public Long id;
+
+    @URL
     @Column(name = "DOCUMENT_URL")
     public String documentUrl;
 
+    @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MIME_TYPE_ID", referencedColumnName = "id")
     public MimeType mimeType;
 
+    @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DOCUMENT_PURPOSE_ID", referencedColumnName = "id")
     public DocumentPurpose documentPurpose;
--- backend/src/main/java/com/prakti/model/Location.java
+++ backend/src/main/java/com/prakti/model/Location.java
@@ -1,13 +1,25 @@
 package com.prakti.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "LOCATION", schema = "PRAKTICOM")
-public class Location extends PanacheEntity {
+public class Location extends PanacheEntityBase {
+
+    @Id
+    @SequenceGenerator(
+            name ="locationSequence",
+            sequenceName = "location_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "locationSequence"
+    )
+    Long id;
 
     @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
