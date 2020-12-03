a0a14718a5ba46f5c7baa26c049e1ed3af5a6b72
eliasroebl <eliasroebl@gmail.com>
2020-12-03 03:09:22 +0100

    updated JobApplication & JobPosting Model

--- backend/src/main/java/com/prakti/model/JobPosting.java
+++ backend/src/main/java/com/prakti/model/JobPosting.java
@@ -1,23 +1,44 @@
 package com.prakti.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "JOB_POSTING", schema = "PRAKTICOM")
-public class JobPosting extends PanacheEntity {
+public class JobPosting extends PanacheEntityBase implements Serializable {
+
+    @Id
+    @SequenceGenerator(
+            name ="jobPostingSequence",
+            sequenceName = "job_posting_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "jobPostingSequence"
+    )
+    Long id;
 
     @Column(name = "JOB_TITLE")
     public String jobTitle;
     @Column(name = "JOB_DESCRIPTION")
+    @Size(max = 2000)
     public String jobDescription;
+
+    @JsonbTransient
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "COMPANY_ID", referencedColumnName = "id")
     public Company company;
+
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     @Column(name = "POSTING_DATE")
     public Date postingDate;
     @Column
@@ -26,15 +47,11 @@ public class JobPosting extends PanacheEntity {
     @OneToMany(mappedBy = "jobPosting", fetch = FetchType.LAZY)
     public List<JobApplication> jobApplications = new ArrayList<>();
 
-    public JobPosting CopyProperties(JobPosting other){
-        if(other.id != null){
-            jobTitle = other.jobTitle;
-            jobDescription = other.jobDescription;
-            company = other.company;
-            postingDate = other.postingDate;
-            timespan = other.timespan;
-        }
-        return this;
+    public void CopyProperties(JobPosting other){
+        this.jobTitle = other.jobTitle;
+        this.jobDescription = other.jobDescription;
+        this.postingDate = other.postingDate;
+        this.timespan = other.timespan;
     }
 
 }
