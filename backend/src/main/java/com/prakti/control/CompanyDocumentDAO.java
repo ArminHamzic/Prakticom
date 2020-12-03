17e1623febeda7001feee11c0e8da4ffe3e7ab0e
eliasroebl <eliasroebl@gmail.com>
2020-12-03 06:07:39 +0100

    first changes in DAOs of Documents

--- backend/src/main/java/com/prakti/control/CompanyDocumentDAO.java
+++ backend/src/main/java/com/prakti/control/CompanyDocumentDAO.java
@@ -3,6 +3,11 @@ package com.prakti.control;
 import com.prakti.model.DocumentEntities.CompanyDocument;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
 public class CompanyDocumentDAO implements PanacheRepositoryBase<CompanyDocument, Long> {
 
     public CompanyDocument persistDocument(CompanyDocument document) {
--- backend/src/main/java/com/prakti/control/StudentDocumentDAO.java
+++ backend/src/main/java/com/prakti/control/StudentDocumentDAO.java
@@ -3,6 +3,11 @@ package com.prakti.control;
 import com.prakti.model.DocumentEntities.StudentDocument;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
 public class StudentDocumentDAO implements PanacheRepositoryBase<StudentDocument, Long> {
 
     public StudentDocument persistDocument(StudentDocument document) {
