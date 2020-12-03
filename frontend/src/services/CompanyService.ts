0aae2de5c333bd0db8bb7604d0983ae3f98664de
eliasroebl <eliasroebl@gmail.com>
2020-12-03 14:15:58 +0100

    added getByEmail to CompanyService

--- frontend/src/services/CompanyService.ts
+++ frontend/src/services/CompanyService.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import {GenericHttpService} from './GenericHttpService/GenericHttpService';
 import {environment} from '../environments/environment';
 import {ICompany} from '../app/contracts/company';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
@@ -14,6 +15,10 @@ export class CompanyService extends GenericHttpService<ICompany, number> {
 
     constructor(protected http: HttpClient) {
         super(http, `${environment.api.baseUrl}/company`);
+    }
+
+    getByEmail(url: string): Observable<ICompany> {
+        return this.http.get<ICompany>(this.base + '/url/' + url);
     }
 
 }
