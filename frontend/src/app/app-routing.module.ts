74c6ec7640f2eba05ab83410d95f09f3475adf76
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-12 16:41:44 +0100

    adding path for student component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './layout/home/home.component';
 import {LandingPageComponent} from './layout/landing-page/landing-page.component';
+import {StudentComponent} from './prakticom/student/student.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home/landing-page', pathMatch: 'full'},
   {path: 'home', component: HomeComponent, children: [
       {path: '', component: LandingPageComponent},
-      {path: 'landing-page', component: LandingPageComponent}],
+      {path: 'landing-page', component: LandingPageComponent},
+      {path: 'student', component: StudentComponent}],
     },
   {path: '**', component: HomeComponent},
 ];
