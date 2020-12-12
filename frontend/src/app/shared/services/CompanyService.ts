a05ae81bf66035fe3a49271be6eeccf3d60aaac0
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-12 15:00:57 +0100

    fixing imports

--- frontend/src/app/shared/services/CompanyService.ts
+++ frontend/src/app/shared/services/CompanyService.ts
@@ -3,14 +3,9 @@ import { HttpClient } from '@angular/common/http';
 
 
 import {GenericHttpService} from './GenericHttpService/GenericHttpService';
-<<<<<<< HEAD:frontend/src/app/shared/services/CompanyService.ts
-import {environment} from '../../../environments/environment';
 import {ICompany} from '../contracts/company';
-=======
-import {environment} from '../environments/environment';
-import {ICompany} from '../app/contracts/company';
 import {Observable} from 'rxjs';
->>>>>>> f3e2dd063002416cd8f26086331ee1ad5a5f2d6b:frontend/src/services/CompanyService.ts
+import {environment} from '../../../environments/environment';
 
 
 @Injectable({
--- frontend/src/app/shared/services/JobPostingService.ts
+++ frontend/src/app/shared/services/JobPostingService.ts
@@ -3,16 +3,9 @@ import { HttpClient } from '@angular/common/http';
 
 
 import {GenericHttpService} from './GenericHttpService/GenericHttpService';
-<<<<<<< HEAD:frontend/src/app/shared/services/JobPostingService.ts
-import {environment} from '../../../environments/environment';
-import {ICompany} from '../contracts/company';
 import {IJobPosting} from '../contracts/jobPosting';
-=======
-import {environment} from '../environments/environment';
-import {ICompany} from '../app/contracts/company';
-import {IJobPosting} from '../app/contracts/jobPosting';
 import {Observable} from 'rxjs';
->>>>>>> f3e2dd063002416cd8f26086331ee1ad5a5f2d6b:frontend/src/services/JobPostingService.ts
+import {environment} from '../../../environments/environment';
 
 
 @Injectable({
