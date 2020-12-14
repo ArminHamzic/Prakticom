6c2205bf713d35cd32c90d81c24236774b8106e6
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-14 13:28:27 +0100

    adding new path for companies component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './layout/home/home.component';
 import {LandingPageComponent} from './layout/landing-page/landing-page.component';
 import {StudentComponent} from './prakticom/student/student.component';
+import {CompaniesComponent} from './prakticom/companies/companies.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home/landing-page', pathMatch: 'full'},
   {path: 'home', component: HomeComponent, children: [
       {path: '', component: LandingPageComponent},
       {path: 'landing-page', component: LandingPageComponent},
+      {path: 'companies', component: CompaniesComponent},
       {path: 'student', component: StudentComponent}],
     },
   {path: '**', component: HomeComponent},
