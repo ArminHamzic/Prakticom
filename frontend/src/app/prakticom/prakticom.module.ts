80f0548581423953d417a6347cb1a97efcd08a8f
Armin Hamzic <arminhamzic2001@hotmail.com>
2020-12-14 13:28:53 +0100

    updating prakticom module

--- frontend/src/app/prakticom/prakticom.module.ts
+++ frontend/src/app/prakticom/prakticom.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentComponent } from './student/student.component';
 import { CompaniesComponent } from './companies/companies.component';
+import {MaterialModule} from '../material.module';
+import { CompanySearchListComponent } from './companies/company-search-list/company-search-list.component';
 
 
 
 @NgModule({
-  declarations: [StudentComponent, CompaniesComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [StudentComponent, CompaniesComponent, CompanySearchListComponent],
+    imports: [
+        CommonModule,
+        MaterialModule
+    ]
 })
 export class PrakticomModule { }
