3b54c574e8a176a218c37aee8eb255d317f3afa1
AlexK2001 <kirchmairalex2001@gmail.com>
2020-12-15 13:12:04 +0100

    updated ICompany in contracts

--- frontend/src/app/shared/contracts/company.ts
+++ frontend/src/app/shared/contracts/company.ts
@@ -1,3 +1,5 @@
+import {ILocation} from './location';
+
 export interface ICompany{
     id: number;
     contactEmail: string;
@@ -5,5 +7,7 @@ export interface ICompany{
     contactPhoneNumber: string;
     description: string;
     name: string;
+    jobPostings: string[];
+    locations: ILocation[];
     url: string;
 }
