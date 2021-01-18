240038d977f9373db76ebd4f86aaca691d04965a
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-01-18 10:22:47 +0100

    fixing bugs

--- frontend/src/app/prakticom/student/student.component.ts
+++ frontend/src/app/prakticom/student/student.component.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
-import { StudentService} from '../../shared/services/StudentService';
-import {IStudent} from '../../shared/contracts/student';
 
 @Component({
   selector: 'app-student',
@@ -13,6 +10,7 @@ export class StudentComponent implements OnInit {
   constructor() {
   }
 
+  skills: string[] = ['Angular', 'Quarkus', '', 'Java', '', 'C#'];
 
   ngOnInit(): void {
 
@@ -36,22 +34,3 @@ export class StudentComponent implements OnInit {
   }*/
   //endregion
 }
-=======
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-student',
-  templateUrl: './student.component.html',
-  styleUrls: ['./student.component.sass']
-})
-export class StudentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  skills: string[] = ['Angular', 'Quarkus', '', 'Java', '', 'C#'];
-
-}
->>>>>>> e2b38ba04a78284544cce49db07cdfa7b7abec78
