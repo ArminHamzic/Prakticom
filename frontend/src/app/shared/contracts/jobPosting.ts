2c86577a901a4724ab1ff9b9ce5c60b44e87a7af
Elias Röbl <eliasroebl@gmail.com>
2021-01-19 10:17:49 +0100

    added FieldOfWorkEnum

--- frontend/src/app/shared/contracts/jobPosting.ts
+++ frontend/src/app/shared/contracts/jobPosting.ts
@@ -9,3 +9,18 @@ export interface IJobPosting{
     // companyId: ICompany;
     companyId: number;
 }
+
+export enum FieldOfWork {
+  Consulting,
+  Coaching,
+  Logistic,
+  Finances,
+  Management,
+  Tourism,
+  Design,
+  IT,
+  Marketing,
+  HumanResources,
+  Production,
+  Research
+}
