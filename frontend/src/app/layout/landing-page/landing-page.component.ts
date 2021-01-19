a5457bbb155f83ca034e5e361068e61d04126905
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-01-19 12:34:11 +0100

    imrpoving search function

--- frontend/src/app/layout/landing-page/landing-page.component.ts
+++ frontend/src/app/layout/landing-page/landing-page.component.ts
@@ -17,16 +17,19 @@ export class LandingPageComponent implements OnInit {
   companies: ICompany[] = [];
   jobPostings: IJobPosting[] = [];
 
+  searchInput: string;
+  searchInput2: string;
+
+
   constructor(private companyService: CompanyService,
               private jobPostingService: JobPostingService) { }
 
   async ngOnInit(): Promise<void> {
-
     const rawCompanies = await this.companyService.getAll().toPromise();
     this.companies = rawCompanies;
     this.matCompanies = new MatTableDataSource<ICompany>(rawCompanies);
+
     const rawJobPostings = await this.jobPostingService.getAll().toPromise();
-    console.log(rawJobPostings);
     this.jobPostings = rawJobPostings;
     this.matJobPostings = new MatTableDataSource<IJobPosting>(rawJobPostings);
 
@@ -39,13 +42,16 @@ export class LandingPageComponent implements OnInit {
       const transformedFilter = filter.trim().toLowerCase();
       return dataStr.indexOf(transformedFilter) !== -1;
     };
-  }
 
-  transform(items: any[], filterQuery: any): any[] {
-    if (!filterQuery) {
-      return items;
-    }
-    return items.filter(item => item.whateverProperty.toLowerCase().includes(filterQuery.toLowerCase()));
+    this.matJobPostings.filterPredicate = (data, filter: string)  => {
+      const accumulator = (currentTerm, key) => {
+        return this.nestedFilterCheck(currentTerm, data, key);
+      };
+      const dataStr = Object.keys(data).reduce(accumulator, '').toLowerCase();
+      // Transform the filter by converting it to lowercase and removing whitespace.
+      const transformedFilter = filter.trim().toLowerCase();
+      return dataStr.indexOf(transformedFilter) !== -1;
+    };
   }
 
   // tslint:disable-next-line:typedef
@@ -62,16 +68,12 @@ export class LandingPageComponent implements OnInit {
     return search;
   }
 
-  applyFilter(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.matCompanies.filter = filterValue.trim().toLowerCase();
-  }
-
-  filterCompanies() {
-
-  }
-
-  filterJobs() {
-
+  filterJobs(): void {
+    if (this.searchInput !== null || this.searchInput2 !== null) {
+      this.matCompanies.filter = this.searchInput.trim().toLowerCase();
+      this.matJobPostings.filter = this.searchInput.trim().toLowerCase();
+      this.matJobPostings.filter = this.searchInput2.trim().toLowerCase();
+      this.matCompanies.filter = this.searchInput2.trim().toLowerCase();
+    }
   }
 }
