4cf71fa0c4a021ca113f3e6cf490bc9a443c6bcc
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-01-26 14:20:48 +0100

    adding some logic

--- frontend/src/app/prakticom/login/register-student/register-student.component.ts
+++ frontend/src/app/prakticom/login/register-student/register-student.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {IStudent} from "../../../shared/contracts/student";
+import {StudentService} from "../../../shared/services/StudentService";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register-student',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterStudentComponent implements OnInit {
 
-  constructor() { }
+  student: IStudent;
+
+  constructor(private studentService: StudentService,
+              private snackBar: MatSnackBar,
+              private router: Router) { }
+
 
   ngOnInit(): void {
+    this.studentService.getAll();
+  }
+
+  /*
+  async onSubmit(): Promise<void> {
+    if (this.employee != null) {
+      await this.employeeService.createUser({body: this.employee}).subscribe(  (response) => {
+        this.snackBar.open('Mitarbeiter wurde erfolgreich angelegt!', 'X', {
+          duration: 8000
+        });
+        this.navigation.back();
+      });
+    }
+  }
+   */
+
+  onClose(): void {
+    this.router.navigate(['/home/login']);
   }
 
 }
--- frontend/src/app/prakticom/prakticom.module.ts
+++ frontend/src/app/prakticom/prakticom.module.ts
@@ -14,17 +14,19 @@ import { CompanyMapComponent } from './companies/company-profile-page/company-ma
 import { LoginComponent } from './login/login.component';
 import { RegisterStudentComponent } from './login/register-student/register-student.component';
 import { RegisterCompanyComponent } from './login/register-company/register-company.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [StudentComponent, CompaniesComponent, CompanySearchListComponent, CompanyProfilePageComponent,
     CompanyInfoCardComponent, CompanyJobAdsSwiperComponent, CompanyMapComponent, LoginComponent, RegisterStudentComponent, RegisterCompanyComponent],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    NgxUsefulSwiperModule,
-    LayoutModule,
-    RouterModule,
-  ]
+    imports: [
+        CommonModule,
+        MaterialModule,
+        NgxUsefulSwiperModule,
+        LayoutModule,
+        RouterModule,
+        FormsModule,
+    ]
 })
 export class PrakticomModule {
 }
