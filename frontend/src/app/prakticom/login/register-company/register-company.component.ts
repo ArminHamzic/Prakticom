68679ae323d9377eb5fdee6174deda398a2dba11
Armin Hamzic <arminhamzic2001@hotmail.com>
2021-02-22 21:30:43 +0100

    adding logic for all buttons & persisting

--- frontend/src/app/prakticom/login/register-company/register-company.component.ts
+++ frontend/src/app/prakticom/login/register-company/register-company.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ICompany} from '../../../shared/contracts/company';
 import {CompanyService} from '../../../shared/services/CompanyService';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
+import {ILocation} from '../../../shared/contracts/location';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-register-company',
@@ -13,33 +16,57 @@ import {MatDialog} from '@angular/material/dialog';
 export class RegisterCompanyComponent implements OnInit {
 
   company: ICompany = {} as ICompany;
+  location: ILocation = undefined;
+  companyLocations: MatTableDataSource<ILocation> = new MatTableDataSource<ILocation>();
+  displayedColumns: string[] = ['address', 'city', 'plz', 'country', 'settings'];
   passwordSafe = '';
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   constructor(private companyService: CompanyService,
               private snackBar: MatSnackBar,
               private router: Router,
-              public dialog: MatDialog) { }
+              public dialog: MatDialog) {
 
-  /*
-  async onSubmit(): Promise<void> {
-    if (this.employee != null) {
-      await this.employeeService.createUser({body: this.employee}).subscribe(  (response) => {
-        this.snackBar.open('Mitarbeiter wurde erfolgreich angelegt!', 'X', {
-          duration: 8000
-        });
-        this.navigation.back();
-      });
-    }
+    this.companyLocations.paginator = this.paginator;
   }
-   */
 
   ngOnInit(): void {
-    this.companyService.getAll();
   }
 
   onClose(): void {
     this.router.navigate(['/home/login']);
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    if (this.company != null) {
+      this.company.locations = this.companyLocations.data;
+      this.companyService.save(this.company).subscribe( (response) => {
+        this.snackBar.open('Unternehmensprofil wurde erfolgreich angelegt!', 'X', {
+          duration: 8000
+        });
+        this.router.navigate(['/home/login']);
+      });
+    }
+  }
+
+  onAddingLocation(): void {
+    if (this.location != null) {
+      this.companyLocations.data.push(this.location);
+      this.companyLocations.data = this.companyLocations.data;
+    }
+    this.location = undefined;
+  }
+
+  onLocationDelete(element): void {
+    this.companyLocations.data.splice(this.companyLocations.data.indexOf(element), 1);
+    this.companyLocations.data = this.companyLocations.data;
+  }
+
+  onCancel(): void {
+    this.location = undefined;
+  }
+
+  onNewLocation(): void {
+    this.location = {} as ILocation;
+  }
 }
