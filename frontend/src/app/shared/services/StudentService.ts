a3cddfb1987f52888f2cd10eee1f0971bb289a29
Elias Röbl <eliasroebl@gmail.com>
2021-02-23 09:49:14 +0100

    added Enum Handling

--- frontend/src/app/shared/services/StudentService.ts
+++ frontend/src/app/shared/services/StudentService.ts
@@ -5,6 +5,8 @@ import { HttpClient } from '@angular/common/http';
 import {GenericHttpService} from './GenericHttpService/GenericHttpService';
 import {IStudent} from '../contracts/student';
 import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {Rating} from '../contracts/rating';
 
 
 @Injectable({
@@ -16,4 +18,20 @@ export class StudentService extends GenericHttpService<IStudent, number> {
         super(http, `${environment.api.baseUrl}/student`);
     }
 
+    save(t: IStudent): Observable<IStudent> {
+
+      const skills: NewSkill[] = [];
+      interface NewSkill{
+        id: number;
+        name: string;
+        rating: string;
+      }
+
+      t.skills.forEach(s => {
+        const newSkill: NewSkill = {id: s.id, name: s.name, rating: Rating[s.rating]};
+        skills.push(newSkill);
+      });
+      t.skills = skills;
+      return super.save(t);
+    }
 }
